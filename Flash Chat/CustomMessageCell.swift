fd57960c048722fbd0e484460225a0abd1dc64fc
David Davidov <D90X@Davids-Air-5.fios-router.home>
2018-03-31 15:41:32 -0400

    Initial Commit

---
+++ Flash Chat/CustomMessageCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomMessageCell.swift
+//  Flash Chat
+//
+//  Created by David Davidov on 12/09/2017.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CustomMessageCell: UITableViewCell {
+
+
+    @IBOutlet var messageBackground: UIView!
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var messageBody: UILabel!
+    @IBOutlet var senderUsername: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code goes here
+        
+        
+        
+    }
+
+
+}
